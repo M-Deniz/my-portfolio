a9d058580b79be62e1a784254f6eaf3012267b8a
M-Deniz <mustafa.deniz01@hotmail.com>
2024-02-28 15:12:45 +0300

    feat(components): add icon button and icon card

---
+++ src/components/VIconButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  icon?: string
+  iconSize?: string
+  alignIconLeft?: boolean
+  renderImage?: boolean
+}>()
+</script>
+
+<template>
+  <button border class="border-color-400 hover-animation" rounded-lg>
+    <div flex items-center justify-center gap-2 px-2 py-1 :class="[alignIconLeft ? 'flex-row-reverse' : 'flex-row']">
+      <span>
+        <slot>
+          VIconButton
+        </slot>
+      </span>
+
+      <div v-if="!renderImage" :class="`${icon} ${iconSize ?? 'text-base'}`" />
+
+      <slot v-else name="icon">
+        Image
+      </slot>
+    </div>
+  </button>
+</template>
---
+++ src/components/VIconCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  alignIconTop?: boolean
+  renderImage?: boolean
+}>()
+</script>
+
+<template>
+  <section class="border-color-400" :class="[alignIconTop ? 'flex-col-reverse' : 'flex-col']" block flex items-center justify-center gap-2 border rounded-lg>
+    <span>
+      <slot>
+        VIconButton
+      </slot>
+    </span>
+
+    <div v-if="!renderImage" :class="icon" text-3xl />
+
+    <slot v-else name="icon">
+      Image
+    </slot>
+  </section>
+</template>
